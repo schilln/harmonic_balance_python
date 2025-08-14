8543838c0fd00f088583abd7b7fb289e9d7a3a3c
Nathan Schill <nateschill@gmail.com>
2025-08-14 10:00:50 -0600

    implemented helper methods

--- src/harmonic_balance/utils.py
+++ src/harmonic_balance/utils.py
@@ -1,3 +1,5 @@
+from collections import abc
+
 import numpy as np
 from scipy import sparse
 
@@ -31,6 +33,28 @@ def extract_dofs_freq(
         return result
 
 
+def combine_dofs_freq(coefficients: ndarray | sparray) -> ndarray | sparray:
+    """Flatten coefficients from `extract_dofs_freq`.
+
+    Parameters
+    ----------
+    coefficients
+        Coefficients where each row corresponds to a degree of freedom
+        shape (n, NH + 1)
+
+    Returns
+    -------
+    reshaped
+        Of the form [a0, a1, ..., aNH] where ak denotes the
+        coefficients of the kth harmonic for the n degrees of freedom.
+    """
+    result = coefficients.reshape((-1,), order="F")
+    if isinstance(result, sparse.coo_array):
+        return result.tocsr()
+    else:
+        return result
+
+
 def extract_dofs_time(time: ndarray | sparray, n: int) -> ndarray | sparray:
     """Reshape time signal by degree of freedom.
 
@@ -49,6 +73,28 @@ def extract_dofs_time(time: ndarray | sparray, n: int) -> ndarray | sparray:
         shape (n, N)
     """
     result = time.reshape((n, -1), order="C")
+    if isinstance(result, sparse.coo_array):
+        return result.tocsr()
+    else:
+        return result
+
+
+def combine_dofs_time(time: ndarray | sparray) -> ndarray | sparray:
+    """Flatten time signal from `extract_dofs_time`.
+
+    Parameters
+    ----------
+    time
+        Time signal where each row corresponds to a degree of freedom
+        shape (n, N)
+
+    Returns
+    -------
+    reshaped
+        Of the form [x0, x1, ..., x(n-1)] where xi denotes the
+        values of the ith degree of freedom for the N time points.
+    """
+    result = time.reshape((-1), order="C")
     if isinstance(result, sparse.coo_array):
         return result.tocsr()
     else:
@@ -76,6 +122,59 @@ def max_abs(
         raise NotImplementedError(
             "`a` must be one of np.ndarray or scipy.sparse.sparray"
         )
+
+
+def get_f_ext(
+    n: int,
+    ks: abc.Iterable[int],
+    dofs: abc.Iterable[int],
+    is_cosines: abc.Iterable[bool],
+    coefficients: abc.Iterable[float],
+) -> abc.Callable[[float, float], ndarray]:
+    """Return a function defining the external force in the time domain.
+
+    See `freq.get_b_ext`.
+
+    If an index is specified more than once (e.g., for both a cosine and sine
+    coefficient), all corresponding coefficients are applied via addition.
+
+    Parameters
+    ----------
+    n
+        Number of degrees of freedom
+    ks
+        The harmonic indices corresponding to each coefficient in `coefficients`
+    dofs
+        Degree of freedom indices corresponding to each coefficient in
+        `coefficients`
+    is_cosines
+        Whether each corresponding coefficient in `coefficients` is cosine
+    coefficients
+        Coefficients of cosine and/or sine in the external force (or constant
+        for k = 0)
+
+    Returns
+    -------
+    f_ext
+        Callable accepting fundamental frequency `omega` and time `t` and
+        returning the external force for the n degrees of freedom in an ndarray
+        of shape (n,)
+    """
+
+    def f_ext(omega, t):
+        res = np.zeros(n)
+        for k, dof, is_cosine, coefficient in zip(
+            ks, dofs, is_cosines, coefficients
+        ):
+            if k == 0:
+                res[dof] += coefficient
+            elif is_cosine:
+                res[dof] += coefficient * np.cos(k * omega * t)
+            else:
+                res[dof] += coefficient * np.sin(k * omega * t)
+        return res
+
+    return f_ext
 
 
 def _max_abs_np(a, axis: int | tuple[int] | None = None) -> ndarray:
