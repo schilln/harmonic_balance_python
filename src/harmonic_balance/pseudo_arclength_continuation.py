d81125fda194a0a36bfc12af428bcc1982521267
Nathan Schill <nateschill@gmail.com>
2025-08-26 12:37:29 -0600

    implemented correction step

--- src/harmonic_balance/pseudo_arclength_continuation.py
+++ src/harmonic_balance/pseudo_arclength_continuation.py
@@ -284,8 +284,8 @@ def compute_tangent(
 
 
 def correct_y(
-    y_i1_k0: sparray | ndarray,
-    y_i0: sparray | ndarray,
+    y_k0: sparray | ndarray,
+    V: sparray | ndarray,
     b_ext: ndarray,
     f_nl: abc.Callable[[ndarray, ndarray, int], ndarray],
     df_nl_dx: abc.Callable[[ndarray, ndarray, int], ndarray],
@@ -293,7 +293,6 @@ def correct_y(
     NH: int,
     n: int,
     N: int,
-    s: float,
     M: ndarray,
     C: ndarray,
     K: ndarray,
@@ -305,10 +304,10 @@ def correct_y(
 
     Parameters
     ----------
-    y_i1_k0
-        Predicted solution y_ik, i = 2, k = 0
-    y_i0
-        Previous solution along nonlinear frequency response curve y_i, i = 0.
+    y_k0
+        Predicted solution
+    V
+        Current tangent vector
     b_ext
         Exponential Fourier coefficients of external force (see `get_b_ext`)
         shape (n * (NH + 1),)
@@ -327,8 +326,6 @@ def correct_y(
         Number of degrees of freedom
     N
         Number of points to sample in time domain
-    s
-        Goal distance between y_i1 and y_i0
     M
         Mass matrix
         shape (n, n)
@@ -354,24 +351,23 @@ def correct_y(
     i
         Number of iterations
     """
-    y = y_i1_k0.copy()
+    y = y_k0.copy()
 
     omega, z = y[-1].real, y[:-1]
     A = freq.get_A(omega, NH, M, C, K)
     R = solve.get_R(z, omega, A, f_nl, b_ext, NH, n, N)
-    P = get_P(y, y_i0, s)
-    rhs = get_rhs(R, P)
-    if get_rel_error(rhs, y) < tol:
-        return y, rhs, True, 0
+    # At the first iteration, the dot product computed by `get_P` is zero since
+    # the difference in the dot product is zero.
+    if get_rel_error(R, y) < tol:
+        return y, R, True, 0
 
     converged = False
     for i in range(max_iter):
-        omega = y[-1].real
-
         A = freq.get_A(omega, NH, M, C, K)
         step = _solve_step(
             y,
-            y_i0,
+            y_k0,
+            V,
             A,
             b_ext,
             f_nl,
@@ -380,7 +376,6 @@ def correct_y(
             NH,
             n,
             N,
-            s,
             M,
             C,
         )
@@ -390,7 +385,7 @@ def correct_y(
         omega, z = y[-1].real, y[:-1]
 
         R = solve.get_R(z, omega, A, f_nl, b_ext, NH, n, N)
-        P = get_P(y, y_i0, s)
+        P = get_P(y, y_k0, V)
         rhs = get_rhs(R, P)
 
         if get_rel_error(rhs, y) < tol:
@@ -419,44 +414,31 @@ def get_rel_error(rhs: ndarray, y: ndarray) -> float:
 
 
 def get_P(
-    y_i1: sparray | ndarray,
-    y_i0: sparray | ndarray,
-    s: float,
+    y: sparray | ndarray,
+    y_k0: sparray | ndarray,
+    V: sparray | ndarray,
 ) -> float:
-    """Compute deviation of distance between y_i1 and y_i0 from the step size s.
+    """Compute dot product of tangent vector with difference between current and
+    predicted solutions.
 
     Parameters
     ----------
-    y_i1, y_i0
-        Solutions y_i = [z_i, omega_i], i = 0, 1
+    y
+        Current estimated solution y = [z, omega]
         shape (n(NH + 1) + 1,)
-    s
-        Goal distance between y_i1 and y_i0
+    y_k0
+        Predicted solution y
+        shape (n(NH + 1) + 1,)
+    V
+        Current tangent vector
+        shape (n(NH + 1) + 1,)
 
     Returns
     -------
     P
-        Residual norm(y_i1 - y_i0)^2 - s^2
+        Dot product V^T (y - y_k0)
     """
-    if isinstance(y_i1, ndarray) or isinstance(y_i0, ndarray):
-        norm = np.linalg.norm
-    else:
-        norm = sparse.linalg.norm
-    return norm(y_i1 - y_i0) ** 2 - s**2
-
-
-def get_dP_dz(
-    z_i1: sparray | ndarray,
-    z_i0: sparray | ndarray,
-) -> sparray | ndarray:
-    return 2 * (z_i1 - z_i0)
-
-
-def get_dP_d_omega(
-    omega_i1: float,
-    omega_i0: float,
-) -> float:
-    return 2 * (omega_i1 - omega_i0)
+    return V @ (y - y_k0)
 
 
 def get_rhs(R: sparray | ndarray, P: float) -> sparray | ndarray:
@@ -464,8 +446,9 @@ def get_rhs(R: sparray | ndarray, P: float) -> sparray | ndarray:
 
 
 def _solve_step(
-    y_i1: ndarray,
-    y_i0: ndarray,
+    y: ndarray,
+    y_k0: ndarray,
+    V: ndarray,
     A: sparray,
     b_ext: ndarray,
     f_nl: abc.Callable[[ndarray, ndarray, int], ndarray],
@@ -474,7 +457,6 @@ def _solve_step(
     NH: int,
     n: int,
     N: int,
-    s: float,
     M: ndarray,
     C: ndarray,
 ) -> ndarray:
@@ -485,30 +467,19 @@ def _solve_step(
     step
         The step to add to y_i1 = [z_i1, omega_i1] to get the (k+1)th correction
     """
-    omega_i1, z_i1 = y_i1[-1].real, y_i1[:-1]
-    omega_i0, z_i0 = y_i0[-1].real, y_i0[:-1]
+    omega, z = y[-1].real, y[:-1]
 
-    R = solve.get_R(z_i1, omega_i1, A, f_nl, b_ext, NH, n, N)
-    P = get_P(y_i1, y_i0, s)
+    R = solve.get_R(z, omega, A, f_nl, b_ext, NH, n, N)
+    P = get_P(y, y_k0, V)
     rhs = get_rhs(R, P)
 
-    db_nl_dz = solve.get_db_nl_dz(
-        omega_i1, z_i1, df_nl_dx, df_nl_d_xdot, NH, n, N
-    )
+    db_nl_dz = solve.get_db_nl_dz(omega, z, df_nl_dx, df_nl_d_xdot, NH, n, N)
     dR_dz = solve.get_dR_dz(A, db_nl_dz)
     dR_d_omega = continuation.get_dR_d_omega(
-        z_i1, omega_i1, df_nl_d_xdot, NH, n, N, M, C
+        z, omega, df_nl_d_xdot, NH, n, N, M, C
     )
 
-    dP_dz = get_dP_dz(z_i1, z_i0)
-    dP_d_omega = get_dP_d_omega(omega_i1, omega_i0)
-
-    jacobian = np.block(
-        [
-            [dR_dz, dR_d_omega.reshape(-1, 1)],
-            [dP_dz.reshape(1, -1), dP_d_omega.reshape(-1, 1)],
-        ]
-    )
+    jacobian = np.block([[dR_dz, dR_d_omega.reshape(-1, 1)], [V]])
 
     step = np.linalg.solve(jacobian, -rhs)
     step[-1] = step[-1].real
